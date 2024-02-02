a4c56b4824d5246a79ea8831784e2b7568241ff1
rspellenberg <rspellenberg@bren.ucsb.edu>
2024-02-02 15:48:04 -0800

    last workshop upload

--- two-file-app/ui.R
+++ two-file-app/ui.R
@@ -1,7 +1,10 @@
 # user interface ----
 ui <- navbarPage(
   
-  theme = "shiny-fresh-theme.css",
+  # theme = "shiny-fresh-theme.css",
+  header = tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "sass-styles.css")
+  ),
   
   title = "LTER Animal Data Explorer",
   
